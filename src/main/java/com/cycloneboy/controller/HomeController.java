40a0be9f0e105844244f32497a06dd7387ceccca
CycloneBoy <xuanfeng1992@gmail.com>
2017-07-16 12:25:10 +0800

    修改home.html 和 subscribe.html

--- src/main/java/com/cycloneboy/controller/HomeController.java
+++ src/main/java/com/cycloneboy/controller/HomeController.java
@@ -4,11 +4,13 @@ import com.cycloneboy.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.jws.WebParam;
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -35,5 +37,25 @@ public class HomeController {
         session.setAttribute("user",new User(1L,"cycloneboy",24));
 
         return  new ModelAndView("home");
+    }
+
+    @RequestMapping(value = "/subscribe",method = RequestMethod.GET)
+    public ModelAndView getSubscribe(){
+
+        return  new ModelAndView("subscribe");
+    }
+
+    @RequestMapping(value = "/subscribe",method = RequestMethod.POST)
+    public String postSubscribe(String email ){
+
+        logger.info("订阅的邮件信息: " + email);
+
+        return "/";
+    }
+
+    @RequestMapping(value = "/userprofile",method = RequestMethod.GET)
+    public ModelAndView getUserprofile(){
+
+        return new ModelAndView("userprofile");
     }
 }
